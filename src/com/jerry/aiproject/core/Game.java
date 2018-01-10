c13d465bce73caadd9325a59e39686de8cd4f165
Jerry <jerryq27@gmail.com>
2018-01-10 16:38:43 +0900

    Fixed up state switching and JPanel rendering by adding a base JPanel for states. Also cleaned up unused code and made comments more readable.

--- src/com/jerry/aiproject/core/Game.java
+++ src/com/jerry/aiproject/core/Game.java
@@ -3,9 +3,7 @@ package com.jerry.aiproject.core;
 import java.awt.*;
 import javax.swing.*;
 
-import com.jerry.aiproject.states.GameState;
-import com.jerry.aiproject.states.PlayState;
-import com.jerry.aiproject.states.MenuState;
+import com.jerry.aiproject.states.*;
 
 /**
  * This class contains the main method.
@@ -17,7 +15,20 @@ import com.jerry.aiproject.states.MenuState;
  * @author Jerry
  */
 public class Game extends JFrame implements Runnable {
-	
+
+    /* **** FIXES **** */
+    /* Centering the JFrame: */
+    //  For setLocationRelativeTo(null) to work in centering:
+    //    - First add the elements to the JFrame.
+    //    - Call pack().
+    //    - Then call setLocationRelativeTo(null) before setVisible(true).
+
+
+    /* GameState Switching & JPanel rendering */
+    //  Adding a third panel to add GameState panels on to:
+    //    - Allows for much easier state switching.
+    //    - Renders the Panels more accurately within the Frame (Used to be off by a few pixels).
+
 	//TODO Make diagonal animation smoother.
 	//TODO Allow for BFS to find a diagonal path.
 	//FIXME Fix up frame collision detection, use the Rectangles?
@@ -30,16 +41,16 @@ public class Game extends JFrame implements Runnable {
     * Enter is being pressed after getting the first item to generate a new path and get the third item,
     * had to increase height of second item to generate collision.*/
 
-	//The dimensions of the window.
-	public static final int WIDTH = 640, HEIGHT = 720; //20*32px X 15*48
+	// The dimensions of the window.
+	public static final int WIDTH = 640, HEIGHT = 720; // 20*32px X 15*48
 	
-	private Thread thread; //The thread the game will be running on.
-	private boolean isRunning; //Flag for determining when the game starts/ends.
-    private boolean inMenu = true; //Flag to determine if the game is in the MenuState.
-    private boolean inGame = false; //Flag to determine if the game is in the PlayState.
-    private GameState currentState; //The current GameState being used.
+	private Thread thread; // The thread the game will be running on.
+	private boolean isRunning; // Flag for determining when the game starts/ends.
+    private GameState currentState; // The current GameState being used.
+    private JPanel baseStatePanel; // Panel to add state panels to.
 
 	public Game() {
+        init();
 		thread = new Thread(this); //Create a new Thread, can use 'this' since Runnable is implemented.
 		thread.start(); //Start the thread AKA call run. 
 	}
@@ -49,42 +60,39 @@ public class Game extends JFrame implements Runnable {
 	 */
 	@Override
 	public void run() {
-		init(); //Initialize variables.
+		// Variables for the Game Loop.
+		long lastTime = System.nanoTime(); // Get the current time.
+		double numberOfUpdates = 60.0; // We want 60 updates per second.
+		double ns = 1000000000/numberOfUpdates; // 1000000000 = 1 second.
+		double delta = 0; // To help determine when to update.
 		
-		//Variables for the Game Loop.
-		long lastTime = System.nanoTime(); //Get the current time.
-		double numberOfUpdates = 60.0; //We want 60 updates per second.
-		double ns = 1000000000/numberOfUpdates; //1000000000 = 1 second.
-		double delta = 0; //To help determine when to update.
-		
-		//Variables for the print out message. 
+		// Variables for the print out message.
 		long timer = System.currentTimeMillis(); //Begin the timer before the loop. 
-		int updates = 0; //Counts the number of updates. 
-		int frames = 0; //Counts the number of frames. 
+		int updates = 0; // Counts the number of updates.
+		int frames = 0; // Counts the number of frames.
 		
-		//Game Loop
+		// Game Loop
 		while(isRunning)
 		{
-			long currentTime = System.nanoTime(); //Get the time.
-			delta += (currentTime - lastTime)/ns; //Subtract the differences of time and divide by '1 second'
-			lastTime = currentTime; //Reset the lastTime value.
+			long currentTime = System.nanoTime(); // Get the time.
+			delta += (currentTime - lastTime)/ns; // Subtract the differences of time and divide by '1 second'
+			lastTime = currentTime; // Reset the lastTime value.
 			
-			while(delta >= 1) //When delta is equal to 1 second.
+			while(delta >= 1) // When delta is equal to 1 second.
 			{
-				update(); //Call update() 60 times a second.
-				updates++; //Increment for print out message. 
-				delta--; //Decrement delta to test for 1 second again. 
+				update(); // Call update() 60 times a second.
+				updates++; // Increment for print out message.
+				delta--; // Decrement delta to test for 1 second again.
 			}
-			//repaint(); //Calls paint, not in loop since we want our computer to draw as fast as possible.
-			render();
-            frames++; //Increment for print out message.
+			render(); // Not in loop since we want our computer to draw as fast as possible.
+            frames++; // Increment for print out message.
 					
-			if(System.currentTimeMillis() - timer > 1000) //Print every second, not in loop so we can get render counts.
+			if(System.currentTimeMillis() - timer > 1000) // Print every second, not in loop so we can get render counts.
 			{
-				timer += 1000; //Add second.
+				timer += 1000; // Add second.
 				System.out.println("FPS: " + frames + " Updates: " + updates);
-				frames = 0; //Reset value for next count.
-				updates = 0; //Reset value for next count.
+				frames = 0; // Reset value for next count.
+				updates = 0; // Reset value for next count.
 			}
 		}
 	}
@@ -97,21 +105,27 @@ public class Game extends JFrame implements Runnable {
         // Set values for the JFrame and game loop.
         setTitle("AI Project");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        isRunning = true; //The game has running.
+        isRunning = true; // The game has running.
+        baseStatePanel = new JPanel();
 
         // By default, the game should start with the menu state.
         currentState = new MenuState(this, WIDTH, HEIGHT); // By default the menu should show up first.
-        add(currentState);
+        baseStatePanel.add(currentState);
+        add(baseStatePanel); // Add the base panel instead of the GameState Panel to fix rendering issues.
         pack();
 
-        // Fix for centering the window: First pack, then set the location, then set the visibility.
         setLocationRelativeTo(null);
-        //setResizable(false);
+        setResizable(false);
         setVisible(true);
 	}
 
 	public void switchStateTo(GameState newState) {
+        baseStatePanel.removeAll();
         currentState = newState;
+        baseStatePanel.add(currentState);
+        pack();
+        baseStatePanel.revalidate();
+        baseStatePanel.repaint();
     }
 	
 	public void update() {
@@ -119,6 +133,7 @@ public class Game extends JFrame implements Runnable {
 	}
 	
 	public void render() {
+        // Calls the GameState's overidden paintComponent method.
         currentState.repaint();
     }
 
