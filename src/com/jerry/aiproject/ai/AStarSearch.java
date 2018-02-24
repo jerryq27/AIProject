46091011b3a72c064d1b47fc5a790fff00e4f4f9
Jerry <jerryq27@gmail.com>
2018-02-24 20:55:58 +0900

    Re-evaluated A* search, more understandable, but it's better to use a priority queue.

--- src/com/jerry/aiproject/ai/AStarSearch.java
+++ src/com/jerry/aiproject/ai/AStarSearch.java
@@ -1,46 +1,38 @@
 package com.jerry.aiproject.ai;
 
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.List;
 
 import com.jerry.aiproject.data.TileMap;
 import com.jerry.aiproject.gameobjects.GameObject;
 
 /**
- * This class will implement the 
- * A* Search algorithm, implementation
- * of this algorithm was broken down by
+ * This class uses the A* algorithm to find
+ * A path between two Game Objects. This
+ * Algorithm works by picking a node with
+ * The lowest cost based on the cost of
+ * Moving from node to node and the
+ * Node's distance from the goal.
+ *
+ * Implementation of this algorithm was broken down by
  * Kevin Glass, owner of Cokeandcode.com.
- * It is like Breadth First Search, but uses
- * a priority queue to sort the open list. The
- * open list is sorted based on the heuristic 
+ * It uses a priority queue to sort the open list.
+ * The open list is sorted based on the heuristic
  * cost of the Nodes.
  * @author Jerry
  */
 public class AStarSearch {
-	
-	private ArrayList<Node> closedList = new ArrayList<Node>(); //List containing searched tiles. 
-	private ArrayList<Node> openList = new ArrayList<Node>(); //List containing unsearched tiles. 
-	private TileMap tileMap; //The map to search. 
-	
-	private boolean diagonalMoves; //If diagonal moves are allowed, default: false;
-	private int maxDepth; //How far to search before giving up. 
-	private Node[][] nodes; //The nodes of the TileMap. 
-	
-	/**
-	 * Constructor for an A* Search. 
-	 * @param map the TileMap. 
-	 * @param depth how far to search before giving up.
-	 * @param allowDiagonal whether or not to allow diagonal tiles to be searched.
-	 */
-	public AStarSearch(TileMap map, int depth, boolean allowDiagonal) {
+
+	private List<Node> visitedList = new ArrayList<>();
+	private List<Node> notVisitedList = new ArrayList<>();
+	private TileMap tileMap;
+	private Node[][] nodes;
+
+	public AStarSearch(TileMap map) {
 		tileMap = map;
-		maxDepth = depth;
-		diagonalMoves = allowDiagonal;
-		
+
+		// Create the grid map.
 		nodes = new Node[tileMap.getRows()][tileMap.getCols()];
-		
-		//Get the nodes array to have the coordinates for each tile. 
 		for(int row = 0; row < tileMap.getRows(); row++)
 		{
 			for(int col = 0; col < tileMap.getCols(); col++)
@@ -49,150 +41,120 @@ public class AStarSearch {
 			}
 		}
 	}
-	
+
 	/**
-	 * This method calculates the heuristic
-	 * needed for the A* algorithm.
-	 * @param object the GameObject doing the search. 
-	 * @param row the tile row. 
-	 * @param col the tile column.
-	 * @param tileR the goal tile row. 
-	 * @param tileC the goal tile column. 
-	 * @return the cost of the move. 
+	 * This method implements the actual A* algorithm
+	 * To find a path between two game objects.
+	 *
+	 * Pick a node according to the value f.
+	 * f = g + h
+	 * g = movement cost to get from the start to the given node.
+	 * h = the estimated cost to move from the given node to the goal.
+	 *
+	 * @param fromObject The object that's searching.
+	 * @param toObject The object to find.
+	 * @return a path between the two points.
 	 */
-	public float getHeuristicCost(GameObject object, int row, int col, int tileR, int tileC) {
-		float newRow = Math.abs(tileR - row);
-		float newCol = Math.abs(tileC - col);
-		
-		float heuristic = newRow + newCol;
-		
-		return heuristic;
-	}
-	
-	/**
-	 * This method does the main work of the A* Algorithm.
-	 * @param objectSearching the object that is going to use the path. 
-	 * @param objectToFind the object being searched for. 
-	 * @return the Path object containing the information about the chosen path.
-	 */
-	public Path findPath(GameObject objectSearching, GameObject objectToFind) {	
-		//if(tileMap.isBlocked(object, goalR, goalC)) //If next tile is blocked, we can't continue;.
-			//return null;
-		
-		//Get the coordinates of each object. 
-		int startR = tileMap.getRow(objectSearching.getY()), 
-			startC = tileMap.getCol(objectSearching.getX());
-		int goalR = tileMap.getRow(objectToFind.getY()),
-			goalC = tileMap.getCol(objectToFind.getX());
-		
-		//Reset values.
-		nodes[startR][startC].cost = 0;
-		nodes[startR][startC].depth = 0;
-		//Clear the lists. 
-		openList.clear();
-		closedList.clear();
-		
-		//Add the first node to the open list. 
-		openList.add(nodes[startR][startC]);
-		sortOpenList(); //This list must be sorted.
-		
-		//First node has no parent. 
-		nodes[startR][startC].parent = null;
-		
-		//Keeps track of the search length, 0 since we haven't started. 
-		int treeDepth = 0; 
-		while(treeDepth < maxDepth && openList.size() != 0)
+	public Path findPath(GameObject fromObject, GameObject toObject) {
+		Path path = new Path();
+
+		// Figure out the start and end point's coordinates.
+		int startRow = tileMap.getRow(fromObject.getY()),
+				startCol = tileMap.getCol(fromObject.getX());
+		int endRow = tileMap.getRow(toObject.getY()),
+				endCol = tileMap.getCol(toObject.getX());
+
+		// Set the start and end nodes.
+		Node startingNode = nodes[startRow][startCol];
+		startingNode.setParent(null);
+		Node endingNode = nodes[endRow][endCol];
+
+		// Set the cost of the starting node to 0 since we haven't moved yet.
+		startingNode.setDistanceFromStart(0);
+		// Also set the total cost with the heuristic.
+		startingNode.setHeuristic(endingNode);
+		startingNode.calculateCost();
+
+		// First area to check is the starting point.
+		notVisitedList.add(startingNode);
+		while(!notVisitedList.isEmpty())
 		{
-			/* Since the open list is sorted, pull out the first node, 
-			 * it is most likely to be the correct node based on the heuristic.
-			 */
-			Node currentNode = openList.get(0);
-			//If the node is our goal, just break, we've reached the destination. 
-			if(currentNode == nodes[goalR][goalC])
-				break;
-			
-			openList.remove(currentNode); //Node explored, remove from the list. 
-			closedList.add(currentNode); //Add to the closed/explored list. 
-		 
-			//Begin searching through the neighbors of the current node.
-			for(int nRow = -1; nRow < 2; nRow++)
+			// FIFO behavior.
+			Node currentNode = notVisitedList.get(0);
+			// If we find the object, break the loop.
+			if(currentNode.compareTo(endingNode) == 0) { break; }
+
+			// Grab the nodes surrounding the current node.
+			ArrayList<Node> neighbors = getNeighbors(currentNode);
+			Node lowestCostNode = neighbors.get(0);
+			for(Node neighbor : neighbors)
 			{
-				for(int nCol = -1; nCol < 2; nCol++)
-				{
-					//0, 0 would be the current node, we want neighbors, so continue.
-					if(nRow == 0 && nCol == 0)
-						continue;
-					
-					//If diagonal moves are not allowed, the only nRow or nCol may be set. 
-					if(!diagonalMoves)
-					{
-						if(nRow != 0 && nCol != 0) //One must be zero, otherwise, continue. 
-							continue;
-					}
-					
-					//Get the location of the neighbor, and evaluate.
-					int neiRow = nRow + currentNode.row;
-					int neiCol = nCol + currentNode.col;
-					
-					//Check if the neighbor is within the map and unblocked. 
-					if(tileMap.isWithinMap(objectSearching, startR, startC, neiRow, neiCol))
-					{
-						
-						float movementCost = currentNode.cost + tileMap.getMoveCost(objectSearching, currentNode.row, 
-								currentNode.col, neiRow, neiCol);
-						Node neighborNode = nodes[neiRow][neiCol];
-						//Node has been visited. 
-						tileMap.pathFinderVisited(neiRow, neiCol);
-						
-						//Based on the cost, this checks if there might be a better path. 
-						//If the calculated cost is less than the neighbor cost. 
-						if(movementCost < neighborNode.cost)
-						{
-							if(openList.contains(neighborNode))
-								openList.remove(neighborNode);
-							if(closedList.contains(neighborNode))
-								closedList.remove(neighborNode);
-						}
-						
-						//If the node has been removed, add it as a next possible step and update the cost. 
-						if(!openList.contains(neighborNode) && !closedList.contains(neighborNode))
-						{
-							neighborNode.cost = movementCost;
-							neighborNode.heuristic = getHeuristicCost(objectSearching, neiRow, neiCol, goalR, goalC);
-							treeDepth = Math.max(treeDepth, neighborNode.setParent(currentNode));
-							openList.add(neighborNode);
-							sortOpenList();
-						}
-					}
-				}
+				neighbor.setDistanceFromStart(currentNode.getDistanceFromStart() + Node.STEP_COST);
+				neighbor.setHeuristic(endingNode);
+				neighbor.calculateCost();
+
+				lowestCostNode = neighbor.getCost() < lowestCostNode.getCost()? neighbor : lowestCostNode;
+			}
+			if(!notVisitedList.contains(lowestCostNode) && !visitedList.contains(currentNode))
+			{
+				lowestCostNode.setParent(currentNode);
+				notVisitedList.add(lowestCostNode);
+
+				notVisitedList.remove(currentNode);
+				visitedList.add(currentNode);
 			}
 		}
-		
-		//If there is nothing left to search, there was no path. 
-		if(nodes[goalR][goalC].parent == null)
-			return null;
-		
-		//The path has been found, use the parent nodes starting from
-		//The current node to determine the path. 
-		Path path = new Path();
-		Node goalNode = nodes[goalR][goalC];
-		
-		while(goalNode != nodes[startR][startC])
+
+		// Create the path.
+		Node currentNode = endingNode;
+		while(currentNode.compareTo(startingNode) != 0)
 		{
-			path.newStartStep(goalNode); //Keep adding previous nodes to the top. 
-			goalNode = goalNode.parent; //Set node to parent node. 
+			path.prependNode(currentNode);
+			currentNode = currentNode.getParent();
 		}
-		path.newStartStep(goalNode); //Add the first step to the path.
-		path.printPath();
-		//Path has been determined. 
+		// path.prependNode(startingNode);
+
+		cleanUp();
 		return path;
 	}
-	
+
 	/**
-	 * The open list (unexplored tiles) 
-	 * should be sorted.
+	 * This method is used to grab the neighbors
+	 * that are surrounding the current node.
+	 * @param node the current node.
+	 * @return the neighbors of the current node.
 	 */
-	public void sortOpenList() {
-		Collections.sort(openList);
+	public ArrayList<Node> getNeighbors(Node node) {
+		ArrayList<Node> neighbors = new ArrayList<>();
+
+		Node north, east, south, west;
+		try { north = nodes[node.getRow() - 1][node.getCol()]; }
+		catch(ArrayIndexOutOfBoundsException e) { north = null; }
+		try { east = nodes[node.getRow()][node.getCol() + 1]; }
+		catch(ArrayIndexOutOfBoundsException e) { east = null; }
+		try { south = nodes[node.getRow() + 1][node.getCol()]; }
+		catch(ArrayIndexOutOfBoundsException e) { south = null; }
+		try { west = nodes[node.getRow()][node.getCol() - 1]; }
+		catch(ArrayIndexOutOfBoundsException e) { west = null; }
+
+		// Check North.
+		if(north != null) { neighbors.add(north); }
+		// East.
+		if(east != null) { neighbors.add(east); }
+		// South.
+		if(south != null) { neighbors.add(south); }
+		// West.
+		if(west != null) { neighbors.add(west); }
+
+		return neighbors;
+	}
+
+	/**
+	 * Housekeeping.
+	 */
+	public void cleanUp() {
+		nodes = null;
+		visitedList.clear();
+		notVisitedList.clear();
 	}
 }
