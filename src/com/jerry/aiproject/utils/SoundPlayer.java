d893862c1b7e9b72fb6736d493d237898e3b7f8e
Jerry <jerryq27@gmail.com>
2018-05-31 01:08:52 -0500

    Using Threads to try fixing sounds not playing, still buggy.

--- src/com/jerry/aiproject/utils/SoundPlayer.java
+++ src/com/jerry/aiproject/utils/SoundPlayer.java
@@ -1,7 +1,11 @@
 package com.jerry.aiproject.utils;
 
 import com.jerry.aiproject.data.Sound;
+import com.jerry.aiproject.data.Sound.SoundType;
 
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 
 /**
@@ -11,6 +15,8 @@ import java.util.HashMap;
  */
 public class SoundPlayer {
 
+    private AudioInputStream audioStream;
+    private Clip audioClip;
     public static final int AX_PICKUP = 0,
         BOW_PICKUP = 1, SWORD_PICKUP = 2, POTION_PICKUP = 3;
 
@@ -26,12 +32,30 @@ public class SoundPlayer {
     }
 
     private void loadSounds() {
-        sounds.put(AX_PICKUP, new Sound("res/ax-pickup.wav"));
-        sounds.put(BOW_PICKUP, new Sound("res/bow-pickup.wav"));
-        sounds.put(SWORD_PICKUP, new Sound("res/sword-pickup.wav"));
+        sounds.put(AX_PICKUP, new Sound("res/ax-pickup.wav", SoundType.EFFECT));
+        sounds.put(BOW_PICKUP, new Sound("res/bow-pickup.wav", SoundType.EFFECT));
+        sounds.put(SWORD_PICKUP, new Sound("res/sword-pickup.wav", SoundType.EFFECT));
     }
 
     public void play(int key) {
-        sounds.get(key).play(false);
+        Sound playSound = sounds.get(key);
+
+        new Thread(() -> {
+            try {
+                audioStream = AudioSystem.getAudioInputStream(playSound.getSoundFile());
+                // Passing null fixed the invalid format issue.
+                audioClip = AudioSystem.getClip(null);
+                audioClip.open(audioStream);
+                audioClip.start();
+            }
+            catch(UnsupportedAudioFileException e) { System.out.println("Audio file not supported: " + e.getMessage()); }
+            catch(IOException e) { System.out.println("Error in loading the file: " + e.getMessage()); }
+            catch(LineUnavailableException e) { e.printStackTrace(); }
+        }).start();
+
+    }
+
+    public void stop() {
+        audioClip.stop();
     }
 }
